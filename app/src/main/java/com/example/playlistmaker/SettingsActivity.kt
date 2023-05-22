b9108b9ecc564495c71cc667533fb89ff0da42e6
SirGeorga <fedins.pochta@gmail.com>
2023-05-22 16:20:22 +0700

    Sprint 8 H/W v1.0

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.bt_settings_back)
+
+        backButton.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+    }
+}
